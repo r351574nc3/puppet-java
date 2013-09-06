2d7bcc4f2270ea4c3bd48878e65a1b400c117997
Przybylski 중광 <r351574nc3@gmail.com>
2013-09-06 03:04:00 -0700

    Upgrading java

--- manifests/init.pp
+++ manifests/init.pp
@@ -10,8 +10,8 @@
 # == Requires:
 #   - Module['Archive']
 class java {
-	$url32 = "http://download.oracle.com/otn-pub/java/jdk/7u7-b10/jdk-7u7-linux-i586.tar.gz"
-	$url64 = "http://download.oracle.com/otn-pub/java/jdk/7u7-b10/jdk-7u7-linux-x64.tar.gz"
+	$url32 = "http://download.oracle.com/otn-pub/java/jdk/7u25-b15/jdk-7u25-linux-i586.tar.gz"
+	$url64 = "http://download.oracle.com/otn-pub/java/jdk/7u25-b15/jdk-7u25-linux-x64.tar.gz"
 
     Exec {
          path => "/home/vagrant/.rvm/gems/ruby-1.9.3-p194/bin:/home/vagrant/.rvm/gems/ruby-1.9.3-p194@global/bin:/home/vagrant/.rvm/rubies/ruby-1.9.3-p194/bin:/home/vagrant/.rvm/bin:/usr/lib64/ccache:/usr/local/bin:/bin:/usr/bin:/usr/local/sbin:/usr/sbin:/sbin:/home/vagrant/.rvm/bin:/sbin:/usr/sbin:/home/vagrant/.local/bin:/home/vagrant/bin"
@@ -28,7 +28,7 @@ class java {
         mode   => 0775,
     }
 
-    archive::download { "jdk-7u7-linux-x64.tar.gz":
+    archive::download { "jdk-7u25-linux-x64.tar.gz":
 	    ensure        => present,
 	    url           => $url32,
 		agent         => "Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.9.1.16) Gecko/20120421 Firefox/11.0",
@@ -37,15 +37,15 @@ class java {
 	    checksum      => false,
     }
 
-    archive::extract { "jdk-7u7-linux-x64" :
+    archive::extract { "jdk-7u25-linux-x64" :
         ensure     => present,
         target     => "/usr/java",
-        require    => Archive::Download["jdk-7u7-linux-x64.tar.gz"]
+        require    => Archive::Download["jdk-7u25-linux-x64.tar.gz"]
     }
 
 	file { '/usr/java/latest':
 		ensure => link,
-		target => '/usr/java/jdk1.7.0_07',
+		target => '/usr/java/jdk1.7.0_25',
 	}
 
     file { '/usr/bin/java':
